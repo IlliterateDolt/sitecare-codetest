9d7a1cd76f846fedac774bb698e1d199b3359ae0
IlliterateDolt <68133548+IlliterateDolt@users.noreply.github.com>
2023-09-17 10:59:15 -0400

    Update and rename update-this-plugin-name.php to sitecare-codetest.php
    renaming core plugin file

---
+++ sitecare-codetest.php
@@ -0,0 +1,25 @@
+<?php
+/**
+* Plugin Name: SiteCare Code Test
+* Plugin URI: https://github.com/IlliterateDolt/sitecare-codetest/
+* Description: SiteCare WordPress Developer Role code test plugin to create a custom Guttenburg Blockm, 
+* Version: 1.0.0
+* Author: FreshySites
+* Author URI: https://freshysites.com/
+* License: GNU v3.0
+*/
+
+if ( ! defined( 'ABSPATH' ) ) {
+	die();
+}
+
+/* SiteCare Code Test Start */
+//______________________________________________________________________________
+
+
+
+
+
+//______________________________________________________________________________
+/* SiteCare Code Test End */
+?>
