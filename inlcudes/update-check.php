1131eeac3cb0b2f42e89f7d0a0ca168b70a6c2a1
IlliterateDolt <68133548+IlliterateDolt@users.noreply.github.com>
2023-09-17 10:55:58 -0400

    Create update-check.php
    Moving the check for updates into the newly created includes folder

---
+++ inlcudes/update-check.php
@@ -0,0 +1,26 @@
+<?php 
+
+// All About Updates
+
+//  Begin Version Control | Auto Update Checker
+require 'plugin-update-checker/plugin-update-checker.php';
+$myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
+// ***IMPORTANT*** Update this path to New Github Repository Master Branch Path
+	'https://github.com/IlliterateDolt/sitecare-codetest',
+	__FILE__,
+// ***IMPORTANT*** Update this to New Repository Master Branch Path
+	'sitecare-codetest'
+);
+//Enable Releases
+$myUpdateChecker->getVcsApi()->enableReleaseAssets();
+//Optional: If you're using a private repository, specify the access token like this:
+//
+//
+//Future Update Note: Comment in these sections and add token and branch information once private git established
+//
+//
+//$myUpdateChecker->setAuthentication('your-token-here');
+//Optional: Set the branch that contains the stable release.
+//$myUpdateChecker->setBranch('stable-branch-name');
+
+?>
